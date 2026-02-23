023c90d7987316a2e848f8b1db1975d2c1233726
Johnsonbai007 <133105483+Johnsonbai007@users.noreply.github.com>
2026-02-23 19:23:40 +0530

    ui to do list compoenet is added

---
+++ app/notes/page.tsx
@@ -0,0 +1,9 @@
+import { TodoList } from "@/components/todo-list";
+
+export default function NotesPage() {
+  return (
+    <div className="min-h-screen bg-background flex items-center justify-center p-4">
+      <TodoList />
+    </div>
+  );
+}
